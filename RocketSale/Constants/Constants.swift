0bf75018bf17f2fda536df3fd1bcdc5bb83384df
rliao147 <ryanliao123@gmail.com>
2019-05-20 17:14:40 -0700

    Added a new color in the constants

--- RocketSale/Constants/Constants.swift
+++ RocketSale/Constants/Constants.swift
@@ -14,6 +14,7 @@ enum Color {
     static let primaryColor = UIColor(red: 0.22, green: 0.58, blue: 0.29, alpha: 1.0)
     static let secondaryColor = UIColor.lightGray
     static let tertiaryColor = #colorLiteral(red: 0.22, green: 0.58, blue: 0.29, alpha: 1.0)
+    static let uglyColor = UIColor(red: 0.11, green: 0.66, blue: 0.33, alpha: 1.0)
 }
 
 enum BaseURLs {
--- RocketSale/Controllers/Login/LoginViewController.swift
+++ RocketSale/Controllers/Login/LoginViewController.swift
@@ -16,6 +16,7 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
 
     /*
     // MARK: - Navigation

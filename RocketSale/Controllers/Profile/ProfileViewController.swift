3db37109c34d4f71c29d1e6ed41ccde55e97ba8b
Ryan Miranda <ryanm7082@gmail.com>
2019-05-27 14:38:45 -0700

    Profile Screen queries profile info to display

--- RocketSale/Controllers/Profile/ProfileViewController.swift
+++ RocketSale/Controllers/Profile/ProfileViewController.swift
@@ -10,25 +10,33 @@ import UIKit
 import Parse
 
 class ProfileViewController: UIViewController {
-
+    @IBOutlet weak var ProfilePic: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var numTransactionsLabel: UILabel!
+    @IBOutlet weak var interestsLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        makeDummyUser()
-        getCurrentUserInformation()
-        // Do any additional setup after loading the view.
+        updateUserInformation()
     }
     
     func makeDummyUser(){
-//        UserDBHelper.createNewUser(email: "ryan@gmail.com", password: "0123") { (error) in
-//            if let error = error {
-//                print("ERROR: \(error.localizedDescription)")
-//            }
-//            else {
-//                print("User created")
-//            }
-//        }
+        UserDBHelper.createNewUser(email: "ryan@gmail.com", password: "0123") { (error) in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            }
+            else {
+                print("User created")
+            }
+        }
+    }
+    
+    func fillDummyUser() {
         UserDBHelper.initializeUserAccountInformation(email: "ryan@gmail.com", phoneNumber: "4081234567", description: "Tester", interests: ["food", "skateboarding"], profilePicture: nil) { (error) in
             if let error = error {
                  print("ERROR: \(error.localizedDescription)")
@@ -39,12 +47,18 @@ class ProfileViewController: UIViewController {
         }
     }
     
-    func getCurrentUserInformation() {
+    func updateUserInformation() {
         UserDBHelper.getUser(email: "ryan@gmail.com") { (error, user) in
             if let error = error {
                 print("ERROR: \(error.localizedDescription)")
             } else {
-                print(user!)
+                let currUser = user!
+        
+                self.usernameLabel.text = currUser.username
+                self.phoneNumberLabel.text = currUser.phoneNumber
+                self.interestsLabel.text = "\((currUser.interests ?? [""]))"
+                self.numTransactionsLabel.text = "\(currUser.numberOfItemsSold + currUser.numberOfItemsBought)"
+                self.descriptionLabel.text = currUser.profileDescription
             }
         }
     }

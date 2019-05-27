974142ebcd97e07a9884775fc0ac5f4be02b79ea
Ryan Miranda <ryanm7082@gmail.com>
2019-05-27 14:01:04 -0700

    Finished UI for Profile Screen

--- RocketSale/Controllers/Profile/ProfileViewController.swift
+++ RocketSale/Controllers/Profile/ProfileViewController.swift
@@ -13,12 +13,34 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        makeDummyUser()
+        getCurrentUserInformation()
         // Do any additional setup after loading the view.
     }
     
+    func makeDummyUser(){
+//        UserDBHelper.createNewUser(email: "ryan@gmail.com", password: "0123") { (error) in
+//            if let error = error {
+//                print("ERROR: \(error.localizedDescription)")
+//            }
+//            else {
+//                print("User created")
+//            }
+//        }
+        UserDBHelper.initializeUserAccountInformation(email: "ryan@gmail.com", phoneNumber: "4081234567", description: "Tester", interests: ["food", "skateboarding"], profilePicture: nil) { (error) in
+            if let error = error {
+                 print("ERROR: \(error.localizedDescription)")
+            }
+            else{
+                print("User info updated")
+            }
+        }
+    }
+    
     func getCurrentUserInformation() {
-        UserDBHelper.getUser(email: (PFUser.current()?.email)!) { (error, user) in
+        UserDBHelper.getUser(email: "ryan@gmail.com") { (error, user) in
             if let error = error {
                 print("ERROR: \(error.localizedDescription)")
             } else {
